d0b9b387aee2a81fc0d1c8026e35e86a0fe28973
rajshaga <vardhan0327@gmail.com>
2021-10-31 23:08:03 -0400

    Steps

--- PDFReader/src/main/java/main/PDFReader.java
+++ PDFReader/src/main/java/main/PDFReader.java
@@ -4,6 +4,14 @@ public class PDFReader {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// read the file 
+		
+		// extract file 
+		
+		// Query and compare the data
+		
+		// validation
 
 	}
 
